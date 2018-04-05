39a55d69011c7d581725c4e10262d9916f98d385
ucupalandi12 <ucupers1297@gmail.com>
2018-04-05 21:43:53 +0700

    Initial commit

---
+++ app/src/main/java/com/androiddevelopment/Kevin/hellotoast/MainActivity.java
@@ -0,0 +1,45 @@
+package com.androiddevelopment.Kevin.hellotoast;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androiddevelopment.Kevin.hellotoast.R;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void showToast(View view) {
+
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void showPoke(View view) {
+
+        Toast toast = Toast.makeText(this, R.string.poke_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+    public void reset(View view) {
+        mCount = 0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
